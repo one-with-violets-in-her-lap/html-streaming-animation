a22dd440df2c60db08bfddc70104cdf9bff11b09
one-with-violets-in-her-lap <83793845+one-with-violets-in-her-lap@users.noreply.github.com>
2025-07-19 15:45:27 +0500

    initial html streaming animation implementation

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,91 @@
 import "@/assets/styles/main.css";
+import samplePageHtml from "@/sample-page.html?raw";
 
-console.log("Hi");
+import { getHtmlElementBySelectorOrThrow } from "@/utils/get-element-or-throw";
+import { wait } from "@/utils/wait";
+
+const STYLES_TO_INJECT_IN_PREVIEW_IFRAME = `
+html, body {
+    overflow: hidden;
+}
+
+body * {
+    opacity: 0;
+    animation-name: appear;
+    animation-duration: 1s;
+    animation-fill-mode: both;
+}
+
+@keyframes appear {
+    0% {
+	opacity: 0;
+    }
+    
+    100% {
+	opacity: 1;
+    }
+}
+`;
+
+window.addEventListener("load", async () => {
+  const heading = getHtmlElementBySelectorOrThrow("#heading");
+  const codeBlockContainer = getHtmlElementBySelectorOrThrow(
+    "#codeBlockContainer",
+  );
+
+  const previewIframe = document.querySelector("#previewContainer iframe");
+
+  if (!(previewIframe instanceof HTMLIFrameElement)) {
+    throw new Error("#previewContainer element must have an iframe");
+  }
+
+  heading.style.opacity = "1";
+  heading.style.transition = "0.5s ease all";
+  heading.style.translate = "none";
+
+  codeBlockContainer.style.opacity = "1";
+  codeBlockContainer.style.filter = "blur(0px)";
+  codeBlockContainer.style.transition = "0.8s ease all";
+
+  await startSampleHtmlStreaming(
+    getHtmlElementBySelectorOrThrow("#codeBlock"),
+    previewIframe,
+    codeBlockContainer.clientHeight,
+  );
+});
+
+async function startSampleHtmlStreaming(
+  codeBlock: HTMLElement,
+  previewIframe: HTMLIFrameElement,
+  codeBlockContainerHeight: number,
+) {
+  const domParser = new DOMParser();
+
+  function updatePreview() {
+    const parsedDocument = domParser.parseFromString(
+      codeBlock.textContent || "",
+      "text/html",
+    );
+    parsedDocument.head.insertAdjacentHTML(
+      "beforeend",
+      `<style>${STYLES_TO_INJECT_IN_PREVIEW_IFRAME}</style>`,
+    );
+
+    previewIframe.style.scale = "100%";
+    previewIframe.srcdoc = parsedDocument.documentElement.innerHTML;
+  }
+
+  const previewUpdateInterval = setInterval(updatePreview, 3000);
+
+  for (const character of samplePageHtml) {
+    codeBlock.textContent += character;
+    codeBlock.style.position = "absolute";
+    codeBlock.style.top =
+      codeBlockContainerHeight - codeBlock.clientHeight + "px";
+
+    await wait(1);
+  }
+
+  clearInterval(previewUpdateInterval);
+  updatePreview();
+}
