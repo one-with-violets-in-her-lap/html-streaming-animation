50f04d8e14213a797c59d16f9e5519e972f099d5
one-with-violets-in-her-lap <83793845+one-with-violets-in-her-lap@users.noreply.github.com>
2025-07-20 13:57:31 +0500

    add gradient stroke

--- src/App.tsx
+++ src/App.tsx
@@ -39,17 +39,30 @@ export function App() {
 
     return (
         <main className="px-6 py-12 grid grid-cols-2 gap-7 min-h-screen max-sm:grid-cols-1 max-sm:py-6">
-            <div>
+            <div className="relative">
                 <motion.h1
                     className="text-6xl font-serif font-bold mb-10 max-sm:text-4xl max-sm:mb-4"
                     initial={{ opacity: 0, y: 50 }}
                     animate={{ opacity: 1, y: 0 }}
-                    transition={{ duration: 0.8, type: "spring" }}
+                    transition={{ duration: 0.8, type: "spring", delay: 0.6 }}
                 >
                     Creating
                 </motion.h1>
 
-                <CodePreview code={code} />
+                <motion.div
+                    initial={{ opacity: 1, filter: "blur(40px)" }}
+                    animate={{ opacity: 1, filter: "blur(0px)" }}
+                    transition={{ duration: 0.5, delay: 0.6 }}
+                >
+                    <CodePreview code={code} />
+                </motion.div>
+
+                <motion.div
+                    className="w-px bg-gradient-to-b from-transparent to-pink-400 absolute top-0 right-0"
+                    initial={{ height: "0px" }}
+                    animate={{ height: "100%" }}
+                    transition={{ duration: 0.5, delay: 0.1 }}
+                ></motion.div>
             </div>
         </main>
     );
