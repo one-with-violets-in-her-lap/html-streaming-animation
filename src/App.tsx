5afe89250e505a283f443d698982598e20e37bb1
one-with-violets-in-her-lap <83793845+one-with-violets-in-her-lap@users.noreply.github.com>
2025-07-20 20:36:36 +0500

    add final preview on completion

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import samplePageHtml from "@/assets/sample-page.html?raw";
 
-import { motion } from "motion/react";
+import { AnimatePresence, motion } from "motion/react";
 import { useEffect, useState } from "react";
 import { CodePreview } from "@/components/code-preview";
 import { PagePreview } from "@/components/page-preview";
 import { MessDrawing } from "@/components/mess-drawing";
 import { wait } from "@/utils/wait";
+import { buildClassName } from "@/utils/class-names";
 
 export function App() {
     const [code, setCode] = useState("");
@@ -22,11 +23,11 @@ export function App() {
             for (
                 let currentCharacterIndex = 0;
                 currentCharacterIndex <= samplePageHtml.length;
-                currentCharacterIndex += 40
+                currentCharacterIndex += 120
             ) {
                 currentCode += samplePageHtml.slice(
                     currentCharacterIndex,
-                    currentCharacterIndex + 40,
+                    currentCharacterIndex + 120,
                 );
                 setCode(currentCode);
 
@@ -51,41 +52,72 @@ export function App() {
     }, []);
 
     return (
-        <main className="px-6 py-12 grid grid-cols-2 gap-7 min-h-screen max-sm:grid-cols-1 max-sm:py-6">
-            <div className="relative">
-                <motion.h1
-                    className="text-6xl font-serif font-bold mb-10 flex items-center gap-x-2 max-sm:text-4xl max-sm:mb-4"
-                    initial={{ opacity: 0, y: 50 }}
-                    animate={{ opacity: 1, y: 0 }}
-                    transition={{ duration: 0.8, type: "spring", delay: 0.7 }}
-                >
-                    <MessDrawing /> Creating
-                </motion.h1>
+        <main>
+            <div
+                className={buildClassName(
+                    "px-6 py-12 grid grid-cols-2 gap-7 min-h-screen max-sm:grid-cols-1 max-sm:py-6 transition-all duration-300",
+                    isCompleted && "scale-40 opacity-0",
+                )}
+            >
+                <div className="relative">
+                    <motion.h1
+                        className="text-6xl font-serif font-bold mb-10 flex items-center gap-x-2 max-sm:text-4xl max-sm:mb-4"
+                        initial={{ opacity: 0, y: 50 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        transition={{
+                            duration: 0.8,
+                            type: "spring",
+                            delay: 0.7,
+                        }}
+                    >
+                        <MessDrawing /> Creating
+                    </motion.h1>
 
-                <motion.div
-                    initial={{ opacity: 1, filter: "blur(40px)" }}
-                    animate={{ opacity: 1, filter: "blur(0px)" }}
-                    transition={{ duration: 0.5, delay: 0.7 }}
-                >
-                    <CodePreview code={code} />
-                </motion.div>
+                    <motion.div
+                        initial={{ opacity: 1, filter: "blur(40px)" }}
+                        animate={{ opacity: 1, filter: "blur(0px)" }}
+                        transition={{ duration: 0.5, delay: 0.7 }}
+                    >
+                        <CodePreview code={code} />
+                    </motion.div>
 
-                <motion.div
-                    className="w-px bg-gradient-to-b from-transparent to-violet-400 absolute top-2 right-0"
-                    initial={{ height: "0px" }}
-                    animate={{ height: "100%" }}
-                    transition={{ duration: 0.5, delay: 0.1 }}
-                ></motion.div>
+                    <motion.div
+                        className="w-px bg-gradient-to-b from-transparent to-violet-400 absolute top-2 right-0"
+                        initial={{ height: "0px" }}
+                        animate={{ height: "100%" }}
+                        transition={{ duration: 0.5, delay: 0.1 }}
+                    ></motion.div>
 
-                <motion.div
-                    className="h-px bg-gradient-to-r from-transparent to-violet-400 absolute -bottom-2 right-0"
-                    initial={{ width: "0px" }}
-                    animate={{ width: "86%" }}
-                    transition={{ duration: 0.5, delay: 0.6 }}
-                ></motion.div>
+                    <motion.div
+                        className="h-px bg-gradient-to-r from-transparent to-violet-400 absolute -bottom-2 right-0"
+                        initial={{ width: "0px" }}
+                        animate={{ width: "86%" }}
+                        transition={{ duration: 0.5, delay: 0.6 }}
+                    ></motion.div>
+                </div>
+
+                <PagePreview code={code} />
             </div>
 
-            <PagePreview code={code} />
+            <AnimatePresence>
+                {isCompleted && (
+                    <motion.div
+                        className="w-full h-full p-10 fixed top-1/2 left-1/2 -translate-1/2"
+                        initial={{ scaleY: 0, opacity: 0 }}
+                        animate={{ opacity: 1, scaleY: 1 }}
+                        transition={{
+                            duration: 0.4,
+                            type: "spring",
+                            delay: 0.3,
+                        }}
+                    >
+                        <iframe
+                            className="bg-violet-100 rounded-xl w-full h-full shadow-xl"
+                            srcDoc={code}
+                        />
+                    </motion.div>
+                )}
+            </AnimatePresence>
         </main>
     );
 }
