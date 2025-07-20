4d3f425abb9696997d315545b6da41510ba9bec7
one-with-violets-in-her-lap <83793845+one-with-violets-in-her-lap@users.noreply.github.com>
2025-07-20 11:01:55 +0500

    fix iframe blinking by applying dom diffs

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import samplePageHtml from "@/sample-page.html?raw";
 
 import { getHtmlElementBySelectorOrThrow } from "@/utils/get-element-or-throw";
 import { wait } from "@/utils/wait";
+import { DiffDOM } from "diff-dom";
 
 const STYLES_TO_INJECT_IN_PREVIEW_IFRAME = `
 html, body {
@@ -12,80 +13,100 @@ html, body {
 body * {
     opacity: 0;
     animation-name: appear;
-    animation-duration: 1s;
+    animation-duration: 0.4s;
     animation-fill-mode: both;
 }
 
 @keyframes appear {
     0% {
+	scale: 0%;
 	opacity: 0;
     }
     
     100% {
+	scale: 100%;
 	opacity: 1;
     }
 }
 `;
 
 window.addEventListener("load", async () => {
-  const heading = getHtmlElementBySelectorOrThrow("#heading");
-  const codeBlockContainer = getHtmlElementBySelectorOrThrow(
-    "#codeBlockContainer",
-  );
+    const heading = getHtmlElementBySelectorOrThrow("#heading");
+    const codeBlockContainer = getHtmlElementBySelectorOrThrow(
+        "#codeBlockContainer",
+    );
 
-  const previewIframe = document.querySelector("#previewContainer iframe");
+    const previewIframe = document.querySelector("#previewContainer iframe");
 
-  if (!(previewIframe instanceof HTMLIFrameElement)) {
-    throw new Error("#previewContainer element must have an iframe");
-  }
+    if (!(previewIframe instanceof HTMLIFrameElement)) {
+        throw new Error("#previewContainer element must have an iframe");
+    }
 
-  heading.style.opacity = "1";
-  heading.style.transition = "0.5s ease all";
-  heading.style.translate = "none";
+    heading.style.opacity = "1";
+    heading.style.transition = "0.5s ease all";
+    heading.style.translate = "none";
 
-  codeBlockContainer.style.opacity = "1";
-  codeBlockContainer.style.filter = "blur(0px)";
-  codeBlockContainer.style.transition = "0.8s ease all";
+    codeBlockContainer.style.opacity = "1";
+    codeBlockContainer.style.filter = "blur(0px)";
+    codeBlockContainer.style.transition = "0.8s ease all";
 
-  await startSampleHtmlStreaming(
-    getHtmlElementBySelectorOrThrow("#codeBlock"),
-    previewIframe,
-    codeBlockContainer.clientHeight,
-  );
+    await startSampleHtmlStreaming(
+        getHtmlElementBySelectorOrThrow("#codeBlock"),
+        previewIframe,
+        codeBlockContainer.clientHeight,
+    );
 });
 
 async function startSampleHtmlStreaming(
-  codeBlock: HTMLElement,
-  previewIframe: HTMLIFrameElement,
-  codeBlockContainerHeight: number,
+    codeBlock: HTMLElement,
+    previewIframe: HTMLIFrameElement,
+    codeBlockContainerHeight: number,
 ) {
-  const domParser = new DOMParser();
+    const domParser = new DOMParser();
+    const domDiff = new DiffDOM();
 
-  function updatePreview() {
-    const parsedDocument = domParser.parseFromString(
-      codeBlock.textContent || "",
-      "text/html",
-    );
-    parsedDocument.head.insertAdjacentHTML(
-      "beforeend",
-      `<style>${STYLES_TO_INJECT_IN_PREVIEW_IFRAME}</style>`,
-    );
+    function updatePreview() {
+        previewIframe.style.scale = "100%";
 
-    previewIframe.style.scale = "100%";
-    previewIframe.srcdoc = parsedDocument.documentElement.innerHTML;
-  }
+        const parsedDocument = domParser.parseFromString(
+            codeBlock.textContent || "",
+            "text/html",
+        );
+        parsedDocument.head.insertAdjacentHTML(
+            "beforeend",
+            `<style>${STYLES_TO_INJECT_IN_PREVIEW_IFRAME}</style>`,
+        );
 
-  const previewUpdateInterval = setInterval(updatePreview, 3000);
+        if (!previewIframe.contentWindow) {
+            throw new Error(
+                "Preview iframe's contentWindow must be accessible by JS",
+            );
+        }
 
-  for (const character of samplePageHtml) {
-    codeBlock.textContent += character;
-    codeBlock.style.position = "absolute";
-    codeBlock.style.top =
-      codeBlockContainerHeight - codeBlock.clientHeight + "px";
+        const diffs = domDiff.diff(
+            previewIframe.contentWindow.document.documentElement,
+            parsedDocument.documentElement,
+        );
 
-    await wait(1);
-  }
+        console.log(diffs);
 
-  clearInterval(previewUpdateInterval);
-  updatePreview();
+        domDiff.apply(
+            previewIframe.contentWindow.document.documentElement,
+            diffs,
+        );
+    }
+
+    const previewUpdateInterval = setInterval(updatePreview, 3000);
+
+    for (const character of samplePageHtml) {
+        codeBlock.textContent += character;
+        codeBlock.style.position = "absolute";
+        codeBlock.style.top =
+            codeBlockContainerHeight - codeBlock.clientHeight + "px";
+
+        await wait(1);
+    }
+
+    clearInterval(previewUpdateInterval);
+    updatePreview();
 }
