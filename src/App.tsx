e305bde7f7db9803eb2325d09f02202d0b0420d9
one-with-violets-in-her-lap <83793845+one-with-violets-in-her-lap@users.noreply.github.com>
2025-07-21 19:29:32 +0500

    add arc vector illustration

--- src/App.tsx
+++ src/App.tsx
@@ -59,7 +59,7 @@ export function App() {
                     isCompleted && "scale-40 opacity-0",
                 )}
             >
-                <div className="relative">
+                <div className="relative pb-10 max-sm:pb-4">
                     <motion.h1
                         className="text-6xl font-serif font-bold mb-10 flex items-center gap-x-2 max-sm:text-4xl max-sm:mb-4"
                         initial={{ opacity: 0, y: 50 }}
@@ -81,17 +81,46 @@ export function App() {
                         <CodePreview code={code} />
                     </motion.div>
 
+                    <svg
+			className="absolute -bottom-2 left-1/2 -translate-x-1/2 w-full max-w-xl stroke-[0.7px] max-sm:h-10"
+                        viewBox="0 0 534 100"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <motion.path
+                            d="M1 99C74.6145 25.0469 284.075 -78.4875 533 99"
+                            stroke="#BDBDBD"
+                            initial={{ pathLength: 0 }}
+                            animate={{ pathLength: 1 }}
+			    transition={{ duration: 1, delay: 0.7 }}
+                        />
+                        <motion.path
+                            d="M219 99C249.996 43.9125 338.189 -33.21 443 99"
+                            stroke="#BDBDBD"
+                            initial={{ pathLength: 0 }}
+                            animate={{ pathLength: 1 }}
+			    transition={{ duration: 1, delay: 0.7 }}
+                        />
+                        <motion.path
+                            d="M111 99C142.687 66.5512 232.85 21.1229 340 99"
+                            stroke="#BDBDBD"
+                            initial={{ pathLength: 0 }}
+                            animate={{ pathLength: 1 }}
+			    transition={{ duration: 1, delay: 0.7 }}
+                        />
+                    </svg>
+
                     <motion.div
-                        className="w-px bg-gradient-to-b from-transparent to-violet-400 absolute top-2 right-0"
+                        className="w-px bg-gradient-to-b from-transparent to-neutral-400 absolute top-2 right-0"
                         initial={{ height: "0px" }}
                         animate={{ height: "100%" }}
                         transition={{ duration: 0.5, delay: 0.1 }}
                     ></motion.div>
 
                     <motion.div
-                        className="h-px bg-gradient-to-r from-transparent to-violet-400 absolute -bottom-2 right-0"
+                        className="h-px bg-gradient-to-r from-neutral-100 to-neutral-300 to-20% absolute -bottom-2 right-0"
                         initial={{ width: "0px" }}
-                        animate={{ width: "86%" }}
+                        animate={{ width: "100%" }}
                         transition={{ duration: 0.5, delay: 0.6 }}
                     ></motion.div>
                 </div>
